3c2a99cc875932ad3d299c1bec65aa82a5ff2492
Cuong Nguyen <cuongchupachups@gmail.com>
2023-03-27 19:48:13 +0700

    setup base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { AppLayout } from './layouts/AppLayout';
 
 function App() {
-  return <AppLayout />;
+  return <h1>Hello world</h1>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import 'antd/dist/reset.css';
+import './index.css';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
