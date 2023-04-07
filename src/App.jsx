809f0ef0be26f88f65f15ae6eed5741cbaeb5087
ĐÌNH AN <115713179+dinhan195@users.noreply.github.com>
2023-04-07 21:14:20 +0700

    card product

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
-import Updateprofile from './pages/Myprofile/Updateprofile/Updateprofile'
-import BannerImg from './pages/Home/Banner/Banner';
-import HomepageProduct from './pages/Home/ShowProducts/HomepageProduct';
-import Footer from './Components/Footer/Footer';
-import  Header  from './Components/Header/Header';
-import SignIn from './pages/Authorization/SignIn/SignIn';
+import ProductCard from './Components/ProductCard/ProductCard'
 function App() {
   return (
     <div>    
-    {/* <SignIn/> */}
-      {/* <Header/> */}
-      <Updateprofile/>
-      {/* <Category/> */}
-      {/* <Footer/> */}
-      {/* <BannerImg/> */}
-      {/* <HomepageProduct/> */}
-      {/* <Footer/> */}
+      <ProductCard/>
+      
     </div>
     
   );
