0d18e597371b0781805c5a5ce2bae787dabdf132
phanhuyduong2003 <huyduongphan8@gmail.com>
2023-04-15 20:41:02 +0700

    Change Password

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import Updateprofile from './pages/Myprofile/Updateprofile/Updateprofile';
+import Updateprofile from './pages/Myprofile/Updateprofile/Updateprofile'
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Changepassword from './pages/Myprofile/Changepassword/Changepassword';
-import ProductCard from './components/ProductCard/ProductCard';
+import ProductCard from './components/ProductCard/ProductCard'
+
 function App() {
+
   return (
-    <div>
-      <Header />
+    <div>    
+    <Header/>
       <Updateprofile />
       <Changepassword/>
+      <ProductCard/>
       <Footer />
-      <ProductCard />
     </div>
+    
   );
 }
 
