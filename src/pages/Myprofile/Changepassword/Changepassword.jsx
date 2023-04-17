164599e1f39afe5a82079de209d87c7b1149d5ff
phanhuyduong2003 <huyduongphan8@gmail.com>
2023-04-17 18:39:13 +0700

    Change Password

--- src/pages/Myprofile/Changepassword/Changepassword.jsx
+++ src/pages/Myprofile/Changepassword/Changepassword.jsx
@@ -1,123 +1,139 @@
 import React from 'react';
-import { Form, Button } from 'antd';
+import { Form, Button, Menu, Layout, Content } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { InputC } from '../../../components/Input/Input';
 import './Changepassword.css';
-import axios from 'axios';
+import axiosInstance from '../../../shared/services/http-client';
 function Changepassword() {
-  const onFinish = event => {
-    event.preventDefault();
-    const { current_password, new_password, repeat_password } = event.target;
-    const data = { current_password: current_password.value, new_password: new_password.value, repeat_password: repeat_password.value };
-    axios.post('/auth/change-password', data).then(response => {});
+  const onFinish = values => {
+    const { current_password, new_password, repeat_password } = values;
+    const data = {
+      currentPassword: current_password,
+      password: new_password,
+      passwordConfirmation: repeat_password,
+    };
+    axiosInstance.post('/auth/change-password', data).then(response => {
+      console.log(response.data);
+      localStorage.setItem('token', response.data);
+    });
   };
   return (
-    <div className="appBg">
-      <Form
-        layout="vertical"
-        name="normal_sigin"
-        className="signin-form"
-        initialValues={{
-          remember: true,
-        }}
-        onFinish={onFinish}
-      >
-        <InputC
-          label="Current password"
-          name="current_password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input Current Password!',
-            },
-            {
-              pattern: /^\S+$/,
-              message: 'Password cannot contain whitespace',
-            },
-          ]}
-          hasFeedback
-          className="formItem"
-          inputClassName="inputField"
-          type="password"
-          iconRender={visible =>
-            visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-          }
-        ></InputC>
-        <InputC
-          label="New password"
-          name="new_password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input New Password!',
-            },
-            {
-              pattern: /^\S+$/,
-              message: 'Password cannot contain whitespace',
-            },
-            {
-              min: 6,
-              message: 'Password must have at least 6 characters in length',
-            },
-            {
-              pattern:
-                /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?]).+$/,
-              message:
-                'Password must contain at least one lowercase, one uppercase letter and one special character',
-            },
-          ]}
-          hasFeedback
-          className="formItem"
-          inputClassName="inputField"
-          type="password"
-          iconRender={visible =>
-            visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-          }
-        ></InputC>
-        <InputC
-          label="Repeat password"
-          name="repeat_password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input Repeat Password!',
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('new_password') === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject(
-                  new Error(
-                    'The passwords that you entered do not match the new passwords'
-                  )
-                );
-              },
-            }),
-            {
-              pattern: /^\S+$/,
-              message: 'Password cannot contain whitespace',
-            },
-          ]}
-          hasFeedback
-          className="formItem"
-          inputClassName="inputField"
-          type="password"
-          iconRender={visible =>
-            visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
-          }
-        ></InputC>
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="changepassword-form-button"
+    <Layout className="change_password">
+        <div className="change_password-nav">
+          <Menu mode="horizontal" className="my_profile-nav">
+            <Menu.Item className="Updateprofile-nav--item" key="myprofile">
+              My profile
+            </Menu.Item>
+            <Menu.Item key="changepassword">Change password</Menu.Item>
+          </Menu>
+        </div>
+        <div className="change_password-form">
+          <Form
+            layout="vertical"
+            name="normal_sigin"
+            className="signin-form"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
           >
-            UPDATE
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+            <InputC
+              label="Current password"
+              name="current_password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Current Password!',
+                },
+                {
+                  pattern: /^\S+$/,
+                  message: 'Password cannot contain whitespace',
+                },
+              ]}
+              hasFeedback
+              className="formItem"
+              inputClassName="inputField"
+              type="password"
+              iconRender={visible =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            ></InputC>
+            <InputC
+              label="New password"
+              name="new_password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input New Password!',
+                },
+                {
+                  pattern: /^\S+$/,
+                  message: 'Password cannot contain whitespace',
+                },
+                {
+                  min: 6,
+                  message: 'Password must have at least 6 characters in length',
+                },
+                {
+                  pattern:
+                    /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+[\]{};':"\\|,.<>/?]).+$/,
+                  message:
+                    'Password must contain at least one lowercase, one uppercase letter and one special character',
+                },
+              ]}
+              hasFeedback
+              className="formItem"
+              inputClassName="inputField"
+              type="password"
+              iconRender={visible =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            ></InputC>
+            <InputC
+              label="Repeat password"
+              name="repeat_password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input Repeat Password!',
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue('new_password') === value) {
+                      return Promise.resolve();
+                    }
+                    return Promise.reject(
+                      new Error(
+                        'The passwords that you entered do not match the new passwords'
+                      )
+                    );
+                  },
+                }),
+                {
+                  pattern: /^\S+$/,
+                  message: 'Password cannot contain whitespace',
+                },
+              ]}
+              hasFeedback
+              className="formItem"
+              inputClassName="inputField"
+              type="password"
+              iconRender={visible =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+            ></InputC>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="changepassword-form-button"
+              >
+                UPDATE
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+    </Layout>
   );
 }
 export default Changepassword;
--- src/pages/Myprofile/Updateprofile/Updateprofile.jsx
+++ src/pages/Myprofile/Updateprofile/Updateprofile.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import './Updateprofile.css';
-import { Layout, Menu} from 'antd';
-import {
-  Button,
-  Form,
-  Input,
-} from 'antd';
+import { Layout, Menu } from 'antd';
+import { Button, Form, Input } from 'antd';
 import { InputC } from '../../../components/Input/Input';
 import { useState } from 'react';
 const { Content, Footer } = Layout;
@@ -26,100 +22,100 @@ const Updateprofile = () => {
             <Menu.Item key="changepassword">Change password</Menu.Item>
           </Menu>
         </div>
- <div className="wrapper-update">
-         <Form
-          layout="vertical"
-          name="normal_update"
-          className="update-form"
-          initialValues={{
-            remember: true,
-          }}
-        //   onFinish={onFinish}
-        >
-          <div className="updateRow">
-          <InputC
-            label="Full name"
-            name="fullname"
-            rules={[
-              {
-                required: true,
-                message: 'Please input Full name!',
-              },
-            ]}
-            className='updateFormItem'
-            inputClassName="updateInputField" type='text'
-          />
-          <InputC
-            label="Username"
-            className="updateFormItem"
-            name="updateUsername"
-            rules={[
-              {
-                required: true,
-                message: 'Please input Username!',
-              },
-            ]}
-            inputClassName="updateInputField" type='text'
-          > 
-          </InputC>
-          </div>
-         <div className="updateRow">
-         <InputC
-            label="Your name"
-            className="updateFormItem"
-            name="updateYourname"
-            rules={[
-              {
-                required: true,
-                message: 'Please input Your name!',
-              },
-            ]}
-            inputClassName="updateInputField" type='text'
+        <div className="wrapper-update">
+          <Form
+            layout="vertical"
+            name="normal_update"
+            className="update-form"
+            initialValues={{
+              remember: true,
+            }}
+            //   onFinish={onFinish}
           >
-          </InputC>
-          <InputC
-            label="Phone number"
-            className="updateFormItem"
-            name="updatePhoneNumber"
-            rules={[
-              {
-                required: true,
-                message: 'Please input Phone number!',
-              },
-            ]}
-            inputClassName="updateInputField" type='text'
-          >
-          </InputC>
-         </div>
-         <div className="updateAddress">
-         <InputC
-            label="Address"
-            className="updateFormItem"
-            name="updateAddress"
-            rules={[
-              {
-                required: true,
-                message: 'Please input Address!',
-              },
-            ]}
-            inputClassName="updateInputField" type='text'
-          >
-          </InputC>
-          
-         </div>
-         <div className="updateBtn">
-         <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="update-form-button"
-            >
-              update
-            </Button>
-          </Form.Item>
-         </div>
-        </Form>
-      </div>
+            <div className="updateRow">
+              <InputC
+                label="Full name"
+                name="fullname"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input Full name!',
+                  },
+                ]}
+                className="updateFormItem"
+                inputClassName="updateInputField"
+                type="text"
+              />
+              <InputC
+                label="Username"
+                className="updateFormItem"
+                name="updateUsername"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input Username!',
+                  },
+                ]}
+                inputClassName="updateInputField"
+                type="text"
+              ></InputC>
+            </div>
+            <div className="updateRow">
+              <InputC
+                label="Your name"
+                className="updateFormItem"
+                name="updateYourname"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input Your name!',
+                  },
+                ]}
+                inputClassName="updateInputField"
+                type="text"
+              ></InputC>
+              <InputC
+                label="Phone number"
+                className="updateFormItem"
+                name="updatePhoneNumber"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input Phone number!',
+                  },
+                ]}
+                inputClassName="updateInputField"
+                type="text"
+              ></InputC>
+            </div>
+            <div className="updateAddress">
+              <InputC
+                label="Address"
+                className="updateFormItem"
+                name="updateAddress"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input Address!',
+                  },
+                ]}
+                inputClassName="updateInputField"
+                type="text"
+              ></InputC>
+            </div>
+            <div className="updateBtn">
+              <Form.Item>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="update-form-button"
+                >
+                  update
+                </Button>
+              </Form.Item>
+            </div>
+          </Form>
+        </div>
       </Content>
     </Layout>
   );
