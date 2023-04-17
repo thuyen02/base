3ca7cd5366bd10461a35b9b38a6c3c63370b1b6b
ĐÌNH AN <115713179+dinhan195@users.noreply.github.com>
2023-04-17 19:17:29 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
-import Category from './pages/Category/Category/Category';
 function App() {
   return (
     <RouterProvider router={router}>
